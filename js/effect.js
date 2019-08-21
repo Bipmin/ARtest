68ea3e0110e62cd0cb7c093ab2da7fc0a9af2856
ryosuke080 <yurilukebody@yahoo.co.jp>
2019-08-22 00:26:46 +0900

    webAR

--- js/effect.js
+++ js/effect.js
@@ -1,29 +1,34 @@
 AFRAME.registerComponent('collide', {
-    init: function() {
-      this.interactiveAnimations();
-      this.el.addEventListener('click', this.onClick);
+        init: function() {
+        this.interactiveAnimations();
+        this.el.addEventListener('click', this.onClick);
     },
+    
+    //クリックしたオブジェクトを回転させる
     interactiveAnimations: function() {
-      this.el.setAttribute('animation__mouseenter', 'property: scale; to: 1.5 1.5 1.5; startEvents: mouseenter; dur: 200');
-      this.el.setAttribute('animation__mouseleave', 'property: scale; to: 1 1 1; startEvents: mouseleave; dur: 200');
-      this.el.setAttribute('animation__click', 'property: scale; to: 3 3 3; startEvents: click; dur:200');
+        this.el.setAttribute('animation__click', 'property:rotation; from:; to: 0 360 0;  startEvents: click; dur:500 repeat: 1;');
     },
+    
+    //クリック位置の取得とパーティクルの設定
     onClick: function(e) {
-      var createEffect = function(point, particleAge) {
+        var createEffect = function(point, particleAge) {
         var pointStr = point.x + ' ' + point.y + ' ' + point.z;
         var effect = document.createElement('a-entity');
         effect.setAttribute('position', pointStr);
         effect.setAttribute('raycaster', 'enabled: false');
-        effect.setAttribute('particle-system', 'color: #ff0, #ff9;maxParticleCount: 100;maxAge: ' + (particleAge / 1000) + ';velocityValue:0 -1 0; accelerationValue: 0 0.5 0; duration: 1;');
+        effect.setAttribute('particle-system', 'preset: defolt; texture: ./images/redheart.png; maxParticleCount: 100;maxAge: ' + (particleAge / 1000) + ';velocityValue:0 -1 0; accelerationValue: 0 0.5 0; duration: 1 repeat: 0; loop: false;');
+        effect.setAttribute('sound', 'src: #decision; autoplay: true; loop: false;');
         return effect;
-      };
-      var point = e.detail.intersection.point;
-      var particleAge = 1500;
-      var effect = createEffect(point, particleAge);
-      var scene = document.querySelector('a-scene');
-      scene.appendChild(effect);
-      setTimeout(function() {
+        };
+
+        //パーティクルを表示させる
+        var point = e.detail.intersection.point;
+        var particleAge = 1500;
+        var effect = createEffect(point, particleAge);
+        var scene = document.querySelector('a-scene');
+        scene.appendChild(effect);
+        setTimeout(function() {
         scene.removeChild(effect);
-      }, particleAge);
+        }, particleAge);
     }
-  });
+});
